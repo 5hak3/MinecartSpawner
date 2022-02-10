f2777f9e0ced28775c11e72b36403d60ec21a95b
5hak3 <shake.salmo@gmail.com>
2022-02-10 20:29:54 +0900

    [FIX] boat despawn condition

--- src/main/java/net/azisaba/tsl/minecartspawner/listener/Despawn.java
+++ src/main/java/net/azisaba/tsl/minecartspawner/listener/Despawn.java
@@ -1,23 +1,18 @@
 package net.azisaba.tsl.minecartspawner.listener;
 
+import org.bukkit.Tag;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.spigotmc.event.entity.EntityDismountEvent;
 
-import java.util.List;
-
 public class Despawn implements Listener {
     @EventHandler
     public void onDismount(EntityDismountEvent event){
-        // Dismoutしたら削除するリスト
-        List<EntityType> dismountor = List.of(
-                EntityType.MINECART,
-                EntityType.BOAT
-        );
-
-        // リストにDismountされたものが含まれていたら処理
-        if (dismountor.contains(event.getDismounted().getType())) {
+        // minecartかボートから降りた時の処理
+        if ( event.getDismounted().getType() == EntityType.MINECART ||
+            (event.getDismounted().getType() == EntityType.BOAT &&
+             Tag.ICE.isTagged(event.getDismounted().getLocation().add(0,-1,0).getBlock().getType()))) {
             // Dismountするときに1つ上の座標にTPする
             event.getEntity().teleport(event.getEntity().getLocation().add(0,1,0));
             event.getDismounted().remove();
